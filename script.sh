fe0f8ba51ca1f69a19cae84ff2d09c8a082c5f76
IrisOlga <ostanulevich@gmail.com>
2021-06-06 18:18:18 +0200

    first script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir homework1
+cd homework1
+mkdir week1 week2 week3
+cd week1
+touch 1.txt 2.txt 3.txt 1.json 2.json
+mkdir day1 day2 day3
+ls -la
+mv /c/Users/User/homework1/week1/1.txt 2.txt /c/Users/User/homework1/week2
+
